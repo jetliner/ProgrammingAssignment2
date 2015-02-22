de6264da2cd0ea818c1a158684366fa7491d8951
Jetliner <otisus@gmx.com>
2015-02-22 20:40:44 +0000

    Inverse Matrix Assignment 2
    functions added to initialize matrix and get/set its inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## provide a set of funcitons to initialize, get, set a matrix
+## and to get and set its inverse. To save processing cycles, if the
+## inverse already caculated, use the cached version.
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  ## create the matrix and access functions
+  ##initialize inverse matrix so clean each invocation
+  ## will initiaize a 1x1 with value NA
+  inverse_matrix <- matrix()
+  ## set the matrix value
+  set <- function(y) {
+    x <<- y
+    inverse_matrix <<- NULL
+  }
+  
+  get <- function() {
+    x
+  }
+  
+  setInverse <- function(z) {
+    inverse_matrix <<- solve(z)
+  }
+  
+  getInverse <- function() {
+    inverse_matrix
+  }
+  ## list() takes tag=value pairs as arguments
+  list(set = set, 
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  ## if the inverse already calculated, use the cache
+  if(!is.na(m)) {
+    message("getting cached inverse matrix")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
 }
